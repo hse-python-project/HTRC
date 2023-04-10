f67e404a7faac65a1189bcc1532f286e0b0daf60
Egor <57009978+trueuser3@users.noreply.github.com>
2023-04-10 23:56:11 +0300

    Update magic.py

--- magic.py
+++ magic.py
@@ -5,3 +5,24 @@ def magic(filename):
     # TODO: the ML stuff
 
     return img
+
+
+import easyocr
+
+def easy_ocr_recognition(file_path):
+  reader = easyocr.Reader(["ru", "en"])
+  result = reader.readtext(file_path, detail = 0)
+  
+  string = ''
+  for k in result:
+    string += k + ' '
+
+  return string
+
+
+def main():
+  file_path = input('Введите путь до файла ')
+  print(easy_ocr_recognition(file_path = file_path))
+
+if __name__ == "__main__":
+  main()
