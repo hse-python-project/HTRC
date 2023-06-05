6ad0da0ec03232d5b43343bdc53135282fcd9677
Egor <57009978+trueuser3@users.noreply.github.com>
2023-06-05 22:48:02 +0300

    Update correction.py

--- correction.py
+++ correction.py
@@ -4,7 +4,6 @@ from langdetect import detect
 
 CORR_KEY = 'bOzCJsRYPqLTPwvy'
 
-
 def correction_with_punctuality(text):
     params = {'text': text, 'language': 'ru-RU', 'ai': 0, 'key': CORR_KEY}
     response = requests.get(url="https://api.textgears.com/grammar", params=params)
@@ -17,6 +16,14 @@ def correction_with_punctuality(text):
         text = text[:mistake['offset']] + mistake['better'][0] + text[mistake['offset'] + mistake['length']:]
     return text
 
+def attention(grammar_fixed_text):
+    ans = []
+    for i in range(len(grammar_fixed_text)):
+        if grammar_fixed_text[i][1] == 1:
+            ans.append('**' + grammar_fixed_text[i][0] + '**')
+        else:
+            ans.append(grammar_fixed_text[i][0])
+    return ans
 
 def correct_text_final(text):
     speller = SpellChecker(language=detect(text))
@@ -24,12 +31,28 @@ def correct_text_final(text):
     grammar_fixed_text = []
     punctual_symbols = [',', '.', '?', '-', '!', '...', ';', ':']
     mistake_counter = 0
+    res = ''
     for word in text.split():
         if word in punctual_symbols:
             grammar_fixed_text.append((word, 0))
         if speller.correction(word) is None:
             grammar_fixed_text.append((word, 0))
         else:
-            grammar_fixed_text.append((speller.correction(word), 1))
-            mistake_counter += 1
-    return (grammar_fixed_text, mistake_counter)
+            temp = speller.correction(word)
+            if word == temp:
+                grammar_fixed_text.append((temp, 0))
+            else:
+                grammar_fixed_text.append((speller.correction(word), 1))
+                mistake_counter += 1
+    teext = attention(grammar_fixed_text)
+    for k in teext:
+        res += k + " "
+    return res
+   
+    
+    
+#tests
+txt = "Thhis exxample for text corection"
+txt1 = 'Это прример для того, чтобы посмотреть как аботает програма'
+correction_with_punctuality(txt)
+print(correct_text_final(txt))
