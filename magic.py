381d8f3318f5555cbcae819ea461f7060aac64f4
sergfedchin <112500418+sergfedchin@users.noreply.github.com>
2023-06-08 00:36:19 +0300

    use implemented correction in magic

--- magic.py
+++ magic.py
@@ -1,28 +1,8 @@
-import easyocr
-from correction import correction_with_punctuality, correct_text_final
+from correction import correction_with_punctuation
 from recognition import recognise, convert_to_text
 
 
-def magic_with_correction(filename):
-    txt = easy_ocr_recognition(filename)
-    first_fix = correction_with_punctuality(txt)
-    result = correct_text_final(first_fix)
-    text = result[0]
-    mistake_counter = result[1]
-    return (text, mistake_counter)
-
-
 def magic(filename, mode):
-    res = recognise(read_path=filename, draw_type='rect')
-    return convert_to_text(res)
-
-
-def easy_ocr_recognition(file_path):
-    reader = easyocr.Reader(["ru", "en"])
-    result = reader.readtext(file_path, detail=0)
-
-    string = ''
-    for k in result:
-        string += k + ' '
-
-    return string
+    pred = recognise(read_path=filename, draw_type='rect')
+    txt = convert_to_text(pred)
+    return correction_with_punctuation(txt) + "\nCORRECTED" if mode == 1 else txt
