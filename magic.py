8e50d8026585391b171dcb3f185418f997e4bad9
sergfedchin <112500418+sergfedchin@users.noreply.github.com>
2023-06-08 19:43:59 +0300

    Update magic.py

--- magic.py
+++ magic.py
@@ -1,8 +1,9 @@
-from correction import correction_with_punctuation
+from correction import correction
 from recognition import recognise, convert_to_text
 
 
 def magic(filename, mode):
     pred = recognise(read_path=filename, draw_type='rect')
     txt = convert_to_text(pred)
-    return correction_with_punctuation(txt) + "\nCORRECTED" if mode == 1 else txt
+    print(txt)
+    return correction(txt) if mode == 1 else txt
