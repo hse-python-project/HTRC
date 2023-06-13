db8e43b3bb2b0b5c1c34889a4f426ce83f6acac2
The Doctor <lemkarik@yandex.ru>
2023-06-13 13:10:23 +0300

    Minor fix

--- magic.py
+++ magic.py
@@ -1,10 +1,10 @@
-from correction import correction, yandex_corr
+from correction import correct
 from recognition import recognise, convert_to_text
 
 
 def magic(filename='', text='', mode=1):
     if mode == 3:
-        return yandex_corr(text)
+        return correct(text)
     pred = recognise(read_path=filename, draw_type='rect')
     txt = convert_to_text(pred)
-    return yandex_corr(txt) if mode != 2 else txt
+    return correct(txt) if mode != 2 else txt
